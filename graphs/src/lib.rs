f72d0bd20a64eb08d71e86a08b43427a88136285
Lakret <Rakuretu@gmail.com>
2021-02-27 22:38:59 +0100

    back to indices

--- graphs/src/lib.rs
+++ graphs/src/lib.rs
@@ -1,15 +1,6 @@
 use derive_more::From;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::rc::Rc;
-
-// position in Graph.vertices
-#[derive(Debug, Eq, PartialEq, Clone, Copy, From, std::hash::Hash)]
-pub struct VId(usize);
-
-// (from, position in Graph.adjacency's vector for from)
-#[derive(Debug, Eq, PartialEq, Clone, Copy)]
-pub struct EId(VId, usize);
 
 // Known approaches:
 //
@@ -35,41 +26,41 @@ trait AbstractGraph<V, E> {
   fn adjacent<X: Iterator<Item = (VId, E)>>(self: &mut Self, vid: VId) -> X;
 }
 
+// position in Graph.vertices
+#[derive(Debug, Eq, PartialEq, Clone, Copy, From, std::hash::Hash)]
+pub struct VId(usize);
+
+// (from, position in Graph.adjacency's vector for from)
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub struct EId(VId, usize);
+
 // Note: currently this cannot support deletion of vertices,
 // since it will shift their positions in the vector.
 #[derive(Debug, Clone)]
 pub struct Graph<V, E> {
-  vertices: HashSet<Rc<V>>,
-  adjacency: HashMap<Rc<V>, Vec<(Rc<V>, E)>>,
+  vertices: Vec<V>,
+  adjacency: HashMap<VId, Vec<(VId, E)>>,
 }
 
-impl<V, E> Graph<V, E>
-where
-  V: Eq + std::hash::Hash,
-{
+impl<V, E> Graph<V, E> {
   pub fn new() -> Self {
     Graph {
-      vertices: HashSet::new(),
+      vertices: vec![],
       adjacency: HashMap::new(),
     }
   }
 
-  pub fn contains_vertex(self: &Self, vertex: &V) -> bool {
-    self.vertices.contains(vertex)
-  }
-
-  pub fn get_adjacent(self: &Self, vertex: &V) -> impl Iterator<Item = &(Rc<V>, E)> {
+  pub fn get_adjacent(self: &Self, vertex: &VId) -> impl Iterator<Item = &(VId, E)> {
     self.adjacency.get(vertex).unwrap().iter()
   }
 
-  pub fn push_vertex(self: &mut Self, vertex: V) -> Rc<V> {
-    let v = Rc::new(vertex);
-    let v2 = Rc::clone(&v);
-    self.vertices.insert(v);
-    v2
+  pub fn push_vertex(self: &mut Self, vertex: V) -> VId {
+    let last_idx = self.vertices.len();
+    self.vertices.push(vertex);
+    VId(last_idx)
   }
 
-  pub fn push_edge(self: &mut Self, from: Rc<V>, to: Rc<V>, edge: E) {
+  pub fn push_edge(self: &mut Self, from: VId, to: VId, edge: E) {
     let adjacent_to_from = self.adjacency.entry(from).or_default();
     adjacent_to_from.push((to, edge));
   }
@@ -86,36 +77,30 @@ mod tests {
     let b_id = g.push_vertex("B");
     let c_id = g.push_vertex("C");
 
-    g.push_edge(a_id.clone(), b_id.clone(), "A -> B".to_string());
-    g.push_edge(b_id.clone(), c_id.clone(), "B -> C".to_string());
-    g.push_edge(c_id.clone(), a_id.clone(), "C -> A".to_string());
-    g.push_edge(a_id.clone(), a_id.clone(), "A loop".to_string());
+    g.push_edge(a_id, b_id, "A -> B".to_string());
+    g.push_edge(b_id, c_id, "B -> C".to_string());
+    g.push_edge(c_id, a_id, "C -> A".to_string());
+    g.push_edge(a_id, a_id, "A loop".to_string());
 
     assert_eq!(g.vertices.len(), 3);
     assert_eq!(
       g.adjacency.get(&a_id).unwrap(),
-      &[
-        (b_id.clone(), "A -> B".to_string()),
-        (a_id.clone(), "A loop".to_string())
-      ]
+      &[(b_id, "A -> B".to_string()), (a_id, "A loop".to_string())]
     );
-    assert_eq!(g.adjacency.get(&b_id).unwrap(), &[(c_id.clone(), "B -> C".to_string())]);
-    assert_eq!(g.adjacency.get(&c_id).unwrap(), &[(a_id.clone(), "C -> A".to_string())]);
+    assert_eq!(g.adjacency.get(&b_id).unwrap(), &[(c_id, "B -> C".to_string())]);
+    assert_eq!(g.adjacency.get(&c_id).unwrap(), &[(a_id, "C -> A".to_string())]);
 
     assert_eq!(
-      g.get_adjacent(&"A").collect::<Vec<_>>(),
-      [
-        &(b_id.clone(), "A -> B".to_string()),
-        &(a_id.clone(), "A loop".to_string())
-      ]
+      g.get_adjacent(&a_id).collect::<Vec<_>>(),
+      [&(b_id, "A -> B".to_string()), &(a_id, "A loop".to_string())]
     );
     assert_eq!(
-      g.get_adjacent(&"B").collect::<Vec<_>>(),
-      [&(c_id.clone(), "B -> C".to_string())]
+      g.get_adjacent(&b_id).collect::<Vec<_>>(),
+      [&(c_id, "B -> C".to_string())]
     );
     assert_eq!(
-      g.get_adjacent(&"C").collect::<Vec<_>>(),
-      [&(a_id.clone(), "C -> A".to_string())]
+      g.get_adjacent(&c_id).collect::<Vec<_>>(),
+      [&(a_id, "C -> A".to_string())]
     );
   }
 }
