aa3af048a7c0740bc3ba0bf15eeb43213ac4638d
Lakret <rakuretu@gmail.com>
2021-02-28 01:46:53 +0100

    indexed graphs sketch

--- graphs/src/lib.rs
+++ graphs/src/lib.rs
@@ -1,6 +1,8 @@
 use derive_more::From;
+use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 
 // Known approaches:
 //
@@ -29,6 +31,76 @@ pub trait AbstractGraph<V, E> {
   where
     F: Fn(&(Self::VId, E)) -> R;
 }
+
+//
+// Indexed Graph
+//
+
+pub struct IGraph<V, E, VId> {
+  vertices: HashMap<VId, V>,
+  adjacency: HashMap<VId, Vec<(VId, E)>>,
+  indexer: Box<dyn Fn(&V) -> VId>,
+}
+
+impl<V, E> IGraph<V, E, u64>
+where
+  V: Hash,
+{
+  pub fn new() -> IGraph<V, E, u64> {
+    IGraph {
+      vertices: HashMap::new(),
+      adjacency: HashMap::new(),
+      indexer: Box::new(|vertex| {
+        let mut state = DefaultHasher::new();
+        vertex.hash(&mut state);
+        state.finish()
+      }),
+    }
+  }
+}
+
+impl<V, E> AbstractGraph<V, E> for IGraph<V, E, u64>
+where
+  V: Hash,
+{
+  type VId = u64;
+
+  fn new() -> Self {
+    IGraph::new()
+  }
+
+  fn push_vertex(self: &mut IGraph<V, E, u64>, vertex: V) -> Self::VId {
+    let vid = (self.indexer)(&vertex);
+    self.vertices.insert(vid, vertex);
+    vid
+  }
+
+  fn push_edge(self: &mut Self, from: Self::VId, to: Self::VId, edge: E) {
+    let adjacent_to_from = self.adjacency.entry(from).or_default();
+    adjacent_to_from.push((to, edge));
+  }
+
+  fn adjacent<'a>(self: &Self, vid: Self::VId) -> Vec<Self::VId> {
+    self.adjacency.get(&vid).unwrap().iter().map(|(vid, _e)| *vid).collect()
+  }
+
+  fn map_adjacent<F, R>(self: &Self, vid: Self::VId, f: F) -> Vec<R>
+  where
+    F: Fn(&(Self::VId, E)) -> R,
+  {
+    self
+      .adjacency
+      .get(&vid)
+      .unwrap()
+      .iter()
+      .map(|vid_and_e| f(vid_and_e))
+      .collect()
+  }
+}
+
+//
+// VecGraph
+//
 
 impl<V, E> AbstractGraph<V, E> for VecGraph<V, E> {
   type VId = usize;
@@ -102,6 +174,34 @@ mod tests {
   #[test]
   fn can_create_a_graph() {
     let mut g: VecGraph<&str, String> = VecGraph::new();
+    let a_id = g.push_vertex("A");
+    let b_id = g.push_vertex("B");
+    let c_id = g.push_vertex("C");
+
+    g.push_edge(a_id, b_id, "A -> B".to_string());
+    g.push_edge(b_id, c_id, "B -> C".to_string());
+    g.push_edge(c_id, a_id, "C -> A".to_string());
+    g.push_edge(a_id, a_id, "A loop".to_string());
+
+    assert_eq!(g.vertices.len(), 3);
+    assert_eq!(
+      g.adjacency.get(&a_id).unwrap(),
+      &[(b_id, "A -> B".to_string()), (a_id, "A loop".to_string())]
+    );
+    assert_eq!(g.adjacency.get(&b_id).unwrap(), &[(c_id, "B -> C".to_string())]);
+    assert_eq!(g.adjacency.get(&c_id).unwrap(), &[(a_id, "C -> A".to_string())]);
+
+    assert_eq!(
+      g.map_adjacent(a_id, |x| x.clone()),
+      [(b_id, "A -> B".to_string()), (a_id, "A loop".to_string())]
+    );
+    assert_eq!(g.map_adjacent(b_id, |x| x.clone()), [(c_id, "B -> C".to_string())]);
+    assert_eq!(g.map_adjacent(c_id, |x| x.clone()), [(a_id, "C -> A".to_string())]);
+  }
+
+  #[test]
+  fn can_create_an_indexed_graph() {
+    let mut g: IGraph<&str, String, u64> = IGraph::new();
     let a_id = g.push_vertex("A");
     let b_id = g.push_vertex("B");
     let c_id = g.push_vertex("C");
