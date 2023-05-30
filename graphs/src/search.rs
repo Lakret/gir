4f734246c21aa8d9ccf8647bc0ccd2f2753d3b5f
Lakret <rakuretu@gmail.com>
2023-05-30 03:24:24 +0200

    options and bfs sketch

--- graphs/src/search.rs
+++ graphs/src/search.rs
@@ -1,8 +1,12 @@
-use std::hash::Hash;
+use std::{
+  collections::{HashMap, HashSet, VecDeque},
+  hash::Hash,
+};
 
 use crate::Graph;
 
 /// Defines a condition for the search to be stopped, i.e. the "goal" or "target" we are searching for.
+#[derive(Debug)]
 pub enum Goal<VId, V = ()> {
   ById(VId),
   ByValue(V),
@@ -11,16 +15,34 @@ pub enum Goal<VId, V = ()> {
 
 /// Defines allowed moves, i.e. for a given `from` vertex, which adjacent vertices can be accessed
 /// by the search procedure.
+#[derive(Debug)]
 pub enum MoveFilter<VId, V = ()> {
   ByIds(fn(from: VId, to: VId) -> bool),
   ByValues(fn(from: V, to: V) -> bool),
 }
 
+#[derive(Debug)]
+pub enum OnExplore<'a, VId> {
+  RecordParents(&'a mut HashMap<VId, VId>),
+}
+
+#[derive(Debug, Default)]
+pub struct Opts<'a, VId, V = ()> {
+  move_filter: Option<MoveFilter<VId, V>>,
+  on_explore: Option<OnExplore<'a, VId>>,
+}
+
 /// Searches the graph `g` using breadth-first search.
-pub fn bfs<VId, E, V>(g: Graph<VId, E, V>, start: VId, goal: Goal<VId, V>, move_filter: Option<MoveFilter<VId, V>>)
+pub fn bfs<VId, E, V>(g: Graph<VId, E, V>, start: VId, goal: Goal<VId, V>, opts: Opts<VId, V>)
 where
-  VId: Eq + Hash,
+  VId: Eq + Hash + Clone,
   V: Hash,
 {
-  todo!()
+  let mut explored = HashSet::new();
+  explored.insert(start.clone());
+
+  let mut queue = VecDeque::new();
+  queue.push_back(start.clone());
+
+  todo!();
 }
