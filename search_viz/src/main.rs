660af8eb7907af4e1e445a5362ab0194445f6ffe
lakret <Rakuretu@gmail.com>
2023-06-08 00:05:47 +0200

    more egui fun

--- search_viz/src/main.rs
+++ search_viz/src/main.rs
@@ -7,9 +7,13 @@ use graphs::Graph;
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
   // Log to stdout (if you run with `RUST_LOG=debug`).
+
+  use eframe::Renderer;
   tracing_subscriber::fmt::init();
 
-  let native_options = eframe::NativeOptions::default();
+  let mut native_options = eframe::NativeOptions::default();
+  // native_options.renderer = Renderer::Wgpu;
+
   eframe::run_native(
     "eframe template",
     native_options,
