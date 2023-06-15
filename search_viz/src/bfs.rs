9d677cf3e0af1cc96d0ffdae63bcad6f9781892a
lakret <Rakuretu@gmail.com>
2023-06-15 17:10:19 +0200

    direct implementation of bfs for the lazy graph of rooms

--- search_viz/src/bfs.rs
+++ search_viz/src/bfs.rs
@@ -1,10 +1,11 @@
 //! AoC 2016, D13, the BFS Example.
 //!
 //! Solution, helper functions, and such.
+use std::collections::{HashMap, HashSet, VecDeque};
 
 const ADJACENT_DELTA: [(i32, i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Pos {
   pub x: u32,
   pub y: u32,
@@ -25,6 +26,48 @@ impl Pos {
     // see https://stackoverflow.com/a/74163801/797544
     (0..32).map(|bit_num| (res >> bit_num) & 1).filter(|v| *v == 1).count() % 2 == 0
   }
+}
+
+/// Searches for `goal` starting at `start` using `fav_number` to check for the walls.
+/// Returns `(parents, is_found)`, where `parents` is a map from a vertex to the vertex it was
+/// first reached from on a path from `start`, and `is_found` is `true` if the `goal` was found.
+pub fn bfs(fav_number: u32, start: Pos, goal: Pos) -> (HashMap<Pos, Pos>, bool) {
+  let mut parents = HashMap::new();
+  let mut queue = VecDeque::from(vec![start]);
+  let mut explored = HashSet::new();
+  explored.insert(start);
+
+  while let Some(curr) = queue.pop_front() {
+    if curr == goal {
+      return (parents, true);
+    }
+
+    for next in curr.adjacent().filter(|next| next.is_open(fav_number)) {
+      if !explored.contains(&next) {
+        explored.insert(next);
+        parents.insert(next, curr);
+        queue.push_back(next);
+      }
+    }
+  }
+
+  (parents, false)
+}
+
+/// Reconstructs path from `start` to `goal` using `parents` returned from `bfs`.
+/// Empty vector means that the `goal` wasn't reached.
+/// The `goal` itself is not included into the returned vector.
+pub fn reconstruct_path(parents: HashMap<Pos, Pos>, goal: Pos) -> Vec<Pos> {
+  let mut path = vec![];
+  let mut curr = goal;
+
+  while let Some(&parent) = parents.get(&curr) {
+    path.push(parent);
+    curr = parent;
+  }
+
+  path.reverse();
+  path
 }
 
 #[cfg(test)]
@@ -61,5 +104,31 @@ mod test {
     assert!((Pos { x: 0, y: 4 }).is_open(10));
     assert!((Pos { x: 1, y: 1 }).is_open(10));
     assert!(!(Pos { x: 2, y: 1 }).is_open(10));
+  }
+
+  #[test]
+  fn bfs_and_reconstruct_path_test() {
+    let goal = Pos { x: 7, y: 4 };
+    let (parents, is_found) = bfs(10, Pos { x: 1, y: 1 }, goal);
+    assert!(is_found);
+
+    let path = reconstruct_path(parents, goal);
+    assert_eq!(
+      path,
+      vec![
+        Pos { x: 1, y: 1 },
+        Pos { x: 1, y: 2 },
+        Pos { x: 2, y: 2 },
+        Pos { x: 3, y: 2 },
+        Pos { x: 3, y: 3 },
+        Pos { x: 3, y: 4 },
+        Pos { x: 4, y: 4 },
+        Pos { x: 4, y: 5 },
+        Pos { x: 5, y: 5 },
+        Pos { x: 6, y: 5 },
+        Pos { x: 7, y: 5 },
+      ]
+    );
+    assert_eq!(path.len(), 11);
   }
 }
