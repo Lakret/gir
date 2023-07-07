e24c7913dd092752a51e94d8a10e221fecb93353
lakret <Rakuretu@gmail.com>
2023-07-07 04:18:02 +0200

    better abstraction

--- search_viz/src/dfs.rs
+++ search_viz/src/dfs.rs
@@ -1,5 +1,42 @@
-use bitvec::prelude::*;
+#[derive(Debug, Clone, Copy)]
+pub enum Mark {
+  Cross,
+  Circle,
+}
 
-#[derive(Debug, Default, Clone, Copy)]
-// `BitArr!(...)` expands to `BitArray<[u16; ::bitvec::mem::elts::<u16>(9)], Msb0>`
-pub struct Game(BitArr!(for 9, in u16, Msb0));
+#[derive(Debug, Default, Clone)]
+pub struct Game {
+  state: [Option<Mark>; 9],
+  circle_turn: bool,
+}
+
+impl Game {
+  pub fn is_cross_turn(&self) -> bool {
+    !self.circle_turn
+  }
+
+  pub fn is_circle_turn(&self) -> bool {
+    self.circle_turn
+  }
+
+  pub fn do_move(&mut self, pos: usize) {
+    assert!(pos < 9);
+
+    let mark = if self.is_cross_turn() {
+      Mark::Cross
+    } else {
+      Mark::Circle
+    };
+
+    self.state[pos] = Some(mark);
+  }
+
+  pub fn next_moves(&self) -> impl Iterator<Item = Game> {
+    todo!();
+    vec![].into_iter()
+  }
+
+  pub fn is_won(&self) -> bool {
+    todo!()
+  }
+}
